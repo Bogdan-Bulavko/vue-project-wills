649e95fffe2ff55bb41f8ab1c18320b0d3ece892
Bogdan <zarechnyy00@internet.ru>
2025-04-29 13:44:12 +0300

    fixed bag style

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -5,10 +5,10 @@ defineProps({ openMenu: Boolean, changeCheckedOpenMenu: Function })
 <template>
   <Transition>
     <nav
-      class="relative max-sm:h-[145px] sm:mr-2.5 z-10 max-sm:absolute max-sm:top-0 max-sm:left-0 max-sm:bg-white max-sm:rounded-t-4xl sm:w-[320px] md:w-100 lg:w-full"
+      class="relative max-sm:h-full sm:mr-2.5 z-10 max-sm:absolute max-sm:top-0 max-sm:left-0 max-sm:bg-white max-sm:rounded-t-4xl sm:w-[320px] md:w-100 lg:w-full"
       v-if="openMenu"
     >
-      <ul class="flex gap-x-5 flex-wrap xs:justify-center min-[350px]:mt-5">
+      <ul class="flex gap-x-5 flex-wrap xs:justify-center max-sm:mt-3">
         <li class="li-item-nav">
           <a href="#" class="a-li-item-nav"
             ><img src="/icons/home.png" alt="home" class="w-[20px] inline" /> Главная</a
@@ -34,10 +34,8 @@ defineProps({ openMenu: Boolean, changeCheckedOpenMenu: Function })
             ><img src="/icons/agenda.png" alt="agenda" class="w-[20px] inline" /> Контакты</a
           >
         </li>
-        <li
-          class="cursor-pointer transition bg-white duration-400 rounded-2xl hover:bg-peach hover:text-white"
-        >
-          <button class="p-2.5">Войти</button>
+        <li class="li-item-nav">
+          <a href="#" class="a-li-item-nav">Войти</a>
         </li>
       </ul>
       <div
